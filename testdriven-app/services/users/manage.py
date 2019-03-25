51fbeb0ef63f9ffecbe3b2beb4c8ff8eb0321ab3
starr499 <robert.vanderstarre@kpn.com>
2019-03-25 09:22:01 +0100

    not working..

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # services/users/manage.py
 
 from flask.cli import FlaskGroup
--- testdriven-app/services/users/project/config.py
+++ testdriven-app/services/users/project/config.py
@@ -1,21 +1,23 @@
 # services/users/project/config.py
-
+import os
 
 class BaseConfig:
     """Basic configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig:
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class TestingConfig:
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
 
 
 class ProductionConfig:
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
